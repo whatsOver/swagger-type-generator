d231e021cb403fb99d1460ab08f3471f68d9a63d
ww8007 <wshmin1234@gmail.com>
2023-05-10 00:35:18 +0900

    feat: inject 형태로 swagger api list 받아올 수 있는 스크립트 추가

---
+++ src/pages/content/modules/getAPIList.ts
@@ -0,0 +1,42 @@
+(async () => {
+  const extractTagsAndEndpoints = async () => {
+    const tagSections = document.querySelectorAll(".opblock-tag-section");
+    const tags = [];
+    const endpoints = {};
+
+    tagSections.forEach((tagSection) => {
+      const tagName = tagSection
+        .querySelector(".opblock-tag")
+        .textContent.trim();
+      tags.push(tagName);
+
+      tagSection.querySelectorAll(".opblock").forEach((opblock) => {
+        const method = opblock.className.split("-")[1].toUpperCase();
+        const path = opblock
+          .querySelector(".opblock-summary-path")
+          .textContent.trim();
+        const description = opblock
+          .querySelector(".opblock-summary-description")
+          .textContent.trim();
+
+        if (!endpoints[tagName]) {
+          endpoints[tagName] = [];
+        }
+
+        endpoints[tagName].push({ method, path, description });
+      });
+    });
+
+    return { tags, endpoints };
+  };
+
+  window.addEventListener("message", async (event) => {
+    if (event.source === window && event.data.type === "GET_API_LIST") {
+      const prList = await extractTagsAndEndpoints();
+      window.postMessage(
+        { type: "GET_API_LIST_RESULT", prList },
+        window.location.origin
+      );
+    }
+  });
+})();
