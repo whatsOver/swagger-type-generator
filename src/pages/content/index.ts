390d4b9a1b8dc5e6c0e269ce7ebef05dcc1e9698
ww8007 <wshmin1234@gmail.com>
2023-05-10 00:35:52 +0900

    feat: PopUp 페이지와 injectScript간 통신 로직 추가

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,7 +1,35 @@
-console.log("content loaded");
+import injectScript from "./modules/injectScript";
 
-/**
- * @description
- * Chrome extensions don't support modules in content scripts.
- */
-import("./components/Demo");
+injectScript(chrome.runtime.getURL("getAPIList.js"), document.documentElement);
+
+const getAPIListFromPage = async (): Promise<
+  Array<{ title: string; issueNumber: string }>
+> => {
+  return new Promise((resolve) => {
+    window.postMessage({ type: "GET_API_LIST" }, window.location.origin);
+    window.addEventListener("message", (event) => {
+      if (
+        event.source === window &&
+        event.data.type === "GET_API_LIST_RESULT"
+      ) {
+        resolve(event.data.prList);
+      }
+    });
+  });
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === "READY") {
+    sendResponse({ code: 200, data: true });
+    return true;
+  }
+  if (request.message === "GET_SWAGGER_LIST") {
+    (async () => {
+      const prList = await getAPIListFromPage();
+      sendResponse({ code: 200, data: prList });
+      return true;
+    })();
+    return true;
+  }
+  return true;
+});
