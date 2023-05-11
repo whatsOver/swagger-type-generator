10808d9b870453ca7db6fe004c50a6fb5b91c611
ww8007 <wshmin1234@gmail.com>
2023-05-12 00:57:11 +0900

    refactor: API 받아오는 로직과 APIItem 컴포넌트 분리 feat: 검색어 기능 추가

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 import { popupStyle } from "./styles/popup.css";
 import { vars } from "./styles/theme.css";
+import Search from "./ui/Search";
+import APIItem from "./ui/APIItem";
+import useGetAPIList from "./hooks/useGetAPIList";
+import useSearch from "./hooks/useSearch";
 
-interface API {
+export interface API {
   method: "GET" | "POST" | "PUT" | "DELETE";
   path: string;
   description: string;
 }
 
-interface APIList {
+export interface APIList {
   endpoints: {
     [key: string]: API[];
   };
@@ -16,87 +20,42 @@ interface APIList {
 }
 
 const Popup = () => {
+  // FIRST RENDER
   const [apiList, setAPIList] = useState<APIList>({
     endpoints: {},
     tags: [],
   });
+  const [filteredAPIList, setFilteredAPIList] = useState<APIList>({
+    endpoints: {},
+    tags: [],
+  });
 
-  const checkIfReceiverIsReady = (
-    tabId: number,
-    callback: (isReady: boolean) => void
-  ) => {
-    chrome.tabs.sendMessage(tabId, { message: "READY" }, (response) => {
-      if (chrome.runtime.lastError) {
-        setTimeout(() => checkIfReceiverIsReady(tabId, callback), 1000);
-      } else {
-        callback(response.data);
-      }
-    });
-  };
+  useGetAPIList({ setAPIList });
 
-  const getAPIList = (tabId: number, callback: (data: APIList) => void) => {
-    chrome.tabs.sendMessage(
-      tabId,
-      { message: "GET_SWAGGER_LIST" },
-      (response) => {
-        if (chrome.runtime.lastError) {
-          console.log(chrome.runtime.lastError);
-          setTimeout(() => getAPIList(tabId, callback), 1000);
-        } else {
-          callback(response.data);
-        }
-      }
-    );
-  };
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      checkIfReceiverIsReady(tabs[0].id, (isReady) => {
-        if (isReady) {
-          getAPIList(tabs[0].id, (data) => {
-            setAPIList(data);
-          });
-        } else {
-          console.error("Error: Receiving end does not exist");
-        }
-      });
-    });
-  }, []);
-
-  console.log(apiList);
+  // INTERACTION
+  // 1. 유저 > 검색어 입력
+  const { search, onChange } = useSearch({ apiList, setFilteredAPIList });
 
   return (
     <div className={popupStyle.app}>
-      <header>
+      <header className={popupStyle.header}>
+        <Search value={search} onChange={onChange} />
         <ul className={popupStyle.apiList}>
-          {apiList.tags.map((tag) => (
+          {filteredAPIList.tags?.map((tag) => (
             <>
-              <h2 className={popupStyle.tag}>{tag}</h2>
+              <h2
+                style={{
+                  color: filteredAPIList.endpoints[tag]?.length
+                    ? vars.color.white
+                    : vars.color.grey,
+                }}
+                className={popupStyle.tag}
+              >
+                {tag}
+              </h2>
               <li className={popupStyle.tagBox} key={tag}>
-                {apiList.endpoints[tag].map((api) => (
-                  <button
-                    style={{
-                      backgroundColor: vars.methodBackgroundColors[api.method],
-                      border: `2px solid ${vars.methodColors[api.method]}`,
-                    }}
-                    className={popupStyle.button}
-                    key={api.path}
-                  >
-                    <div className={popupStyle.flexRow}>
-                      <div
-                        style={{
-                          backgroundColor: vars.methodColors[api.method],
-                        }}
-                        className={popupStyle.methodItem}
-                      >
-                        {api.method}
-                      </div>
-                      <span className={popupStyle.path}>{api.path}</span>
-                    </div>
-                    <span className={popupStyle.description}>
-                      {api.description}
-                    </span>
-                  </button>
+                {filteredAPIList.endpoints[tag]?.map((api) => (
+                  <APIItem api={api} />
                 ))}
               </li>
             </>
---
+++ src/pages/popup/hooks/useGetAPIList.ts
@@ -0,0 +1,67 @@
+import React from "react";
+import { SetStateAction } from "react";
+import { Dispatch } from "react";
+import { useEffect } from "react";
+
+export interface API {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  path: string;
+  description: string;
+}
+
+interface APIList {
+  endpoints: {
+    [key: string]: API[];
+  };
+  tags: string[];
+}
+
+interface GetAPIListProps {
+  setAPIList: Dispatch<SetStateAction<APIList>>;
+}
+
+const useGetAPIList = ({ setAPIList }: GetAPIListProps) => {
+  const checkIfReceiverIsReady = (
+    tabId: number,
+    callback: (isReady: boolean) => void
+  ) => {
+    chrome.tabs.sendMessage(tabId, { message: "READY" }, (response) => {
+      if (chrome.runtime.lastError) {
+        setTimeout(() => checkIfReceiverIsReady(tabId, callback), 1000);
+      } else {
+        callback(response.data);
+      }
+    });
+  };
+
+  const getAPIList = (tabId: number, callback: (data: APIList) => void) => {
+    chrome.tabs.sendMessage(
+      tabId,
+      { message: "GET_SWAGGER_LIST" },
+      (response) => {
+        if (chrome.runtime.lastError) {
+          console.log(chrome.runtime.lastError);
+          setTimeout(() => getAPIList(tabId, callback), 1000);
+        } else {
+          callback(response.data);
+        }
+      }
+    );
+  };
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      checkIfReceiverIsReady(tabs[0].id, (isReady) => {
+        if (isReady) {
+          getAPIList(tabs[0].id, (data) => {
+            setAPIList(data);
+          });
+        } else {
+          console.error("Error: Receiving end does not exist");
+        }
+      });
+    });
+  }, []);
+};
+
+export default useGetAPIList;
