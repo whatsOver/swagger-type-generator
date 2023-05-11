be17ee604d9ba0354aa3543c1279c99190db9502
ww8007 <wshmin1234@gmail.com>
2023-05-12 00:56:06 +0900

    feat: Search UI 컴포넌트 추가

---
+++ src/pages/popup/ui/Search.tsx
@@ -0,0 +1,20 @@
+import Input, { InputProps } from "@src/common/ui/Input";
+import { style } from "@vanilla-extract/css";
+import { BiSearch } from "react-icons/bi";
+import { searchStyle } from "./styles/search.css";
+import { vars } from "../styles/theme.css";
+
+const Search = ({ value, onChange }: InputProps) => {
+  return (
+    <section className={searchStyle.search}>
+      <BiSearch color={vars.color.white} className={searchStyle.icon} />
+      <Input
+        value={value}
+        onChange={onChange}
+        style={{ paddingLeft: "35px" }}
+      />
+    </section>
+  );
+};
+
+export default Search;
