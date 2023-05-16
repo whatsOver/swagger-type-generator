c80865edba5c0d7a1385033e18940bc1dabbae1e
ww8007 <wshmin1234@gmail.com>
2023-05-17 02:44:36 +0900

    feat: Modal 공통 UI 컴포넌트 추가

---
+++ src/common/ui/Modal.tsx
@@ -0,0 +1,106 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { useState, useEffect, useMemo, useRef, createContext } from "react";
+import { modalStyle } from "./styles/modal.css";
+import { useContext } from "react";
+import { cloneElement } from "react";
+
+const ModalContext = createContext<{
+  modalOpen: boolean;
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}>({
+  modalOpen: false,
+  setModalOpen: () => {},
+});
+
+interface ModalProps {
+  children: React.ReactNode;
+}
+
+const Modal = ({ children }: ModalProps) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    const keydownCB = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setModalOpen(false);
+      }
+    };
+
+    if (modalOpen) {
+      document.addEventListener("keydown", keydownCB);
+    } else {
+      document.removeEventListener("keydown", keydownCB);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", keydownCB);
+    };
+  }, [modalOpen]);
+
+  const contextValue = useMemo(
+    () => ({ modalOpen, setModalOpen }),
+    [modalOpen, setModalOpen]
+  );
+
+  return (
+    <ModalContext.Provider value={contextValue}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+interface TriggerProps {
+  as: React.ReactElement;
+}
+
+const Trigger = ({ as }: TriggerProps) => {
+  const { modalOpen, setModalOpen } = useContext(ModalContext);
+
+  const clonedTrigger = cloneElement(as, {
+    onClick: () => {
+      as.props.onClick();
+      setModalOpen(!modalOpen);
+    },
+  });
+  return clonedTrigger;
+};
+
+interface ContentProps {
+  children: React.ReactNode;
+}
+
+const Content = ({ children }: ContentProps) => {
+  const { modalOpen, setModalOpen } = useContext(ModalContext);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  if (!modalOpen) {
+    return null;
+  }
+
+  return (
+    <div
+      className={modalStyle.backdropStyle}
+      onClick={() => setModalOpen(false)}
+      style={{
+        animation: `${
+          modalOpen ? modalStyle.fadeIn : modalStyle.fadeOut
+        } 500ms`,
+      }}
+    >
+      <section
+        className={modalStyle.modalStyle}
+        ref={contentRef}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        {children}
+      </section>
+    </div>
+  );
+};
+
+Modal.Trigger = Trigger;
+Modal.Content = Content;
+
+export default Modal;
