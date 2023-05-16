37f06280d9e949363b540c7be6a6d5fcbeda38bd
ww8007 <wshmin1234@gmail.com>
2023-05-17 02:46:53 +0900

    feat: Swagger API 연동 및 라우팅 로직 추가

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { popupStyle } from "./styles/popup.css";
-import { vars } from "./styles/theme.css";
 import Search from "./ui/Search";
 import APIItem from "./ui/APIItem";
 import useGetAPIList from "./hooks/useGetAPIList";
 import useSearch from "./hooks/useSearch";
+import { useGETDocs } from "./api/docs";
+import { RequestProps } from "./ui/Request";
+import { vars } from "@src/common/ui/styles/theme.css";
+import { Method } from "axios";
+import { useNavigate } from "react-router-dom";
 
 export interface API {
   method: "GET" | "POST" | "PUT" | "DELETE";
@@ -20,11 +24,15 @@ export interface APIList {
 }
 
 const Popup = () => {
+  const { data } = useGETDocs();
+  const navigate = useNavigate();
+
   // FIRST RENDER
   const [apiList, setAPIList] = useState<APIList>({
     endpoints: {},
     tags: [],
   });
+
   const [filteredAPIList, setFilteredAPIList] = useState<APIList>({
     endpoints: {},
     tags: [],
@@ -35,6 +43,44 @@ const Popup = () => {
   // INTERACTION
   // 1. 유저 > 검색어 입력
   const { search, onChange } = useSearch({ apiList, setFilteredAPIList });
+  const [selectedAPI, setSelectedAPI] = useState<RequestProps>(null);
+
+  const onClickAPI = (api: API) => {
+    const removeIs = (str: string) => str.split(" ")[0];
+    const method = removeIs(api.method) as Method;
+    setSelectedAPI((prev) => ({
+      ...prev,
+      method,
+      path: api.path,
+    }));
+    if (data) {
+      const parameter =
+        data.paths[api.path][method.toLowerCase() as Method].parameters;
+      setSelectedAPI((prev) => ({
+        ...prev,
+        params: parameter,
+      }));
+      if (data.paths[api.path][method.toLowerCase()].requestBody) {
+        const schemaName =
+          data.paths[api.path][
+            method.toLowerCase() as Method
+          ].requestBody.content["application/json"].schema.$ref.split("/")[3];
+        const body = data.components.schemas[schemaName];
+        setSelectedAPI((prev) => ({
+          ...prev,
+          body,
+        }));
+      }
+    }
+    navigate("/request", {
+      state: {
+        method,
+        path: api.path,
+        params: selectedAPI.params,
+        body: selectedAPI.body,
+      },
+    });
+  };
 
   return (
     <div className={popupStyle.app}>
@@ -55,7 +101,11 @@ const Popup = () => {
               </h2>
               <li className={popupStyle.tagBox} key={tag}>
                 {filteredAPIList.endpoints[tag]?.map((api) => (
-                  <APIItem api={api} />
+                  <APIItem
+                    key={api.path}
+                    api={api}
+                    onClick={() => onClickAPI(api)}
+                  />
                 ))}
               </li>
             </>
