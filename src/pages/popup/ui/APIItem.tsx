9a4e2b96c1d318fd8e9d0e1f165e71c977561601
ww8007 <wshmin1234@gmail.com>
2023-05-17 02:48:32 +0900

    feat: 단일 API onClick props 추가

--- src/pages/popup/ui/APIItem.tsx
+++ src/pages/popup/ui/APIItem.tsx
@@ -1,14 +1,16 @@
-import { vars } from "../styles/theme.css";
+import { vars } from "@src/common/ui/styles/theme.css";
 import { API } from "../Popup";
 import { popupStyle } from "../styles/popup.css";
 
 interface APIItemProps {
   api: API;
+  onClick: (api: API) => void;
 }
 
-const APIItem = ({ api }: APIItemProps) => {
+const APIItem = ({ api, onClick }: APIItemProps) => {
   return (
     <button
+      onClick={() => onClick(api)}
       style={{
         backgroundColor: vars.methodBackgroundColors[api.method],
         border: `2px solid ${vars.methodColors[api.method]}`,
