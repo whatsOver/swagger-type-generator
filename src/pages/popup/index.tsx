27f0360ac72c951f27342610bf5b4bd6f458c7b0
ww8007 <wshmin1234@gmail.com>
2023-05-17 02:45:39 +0900

    feat: MemoryRouter 추가

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import "@pages/popup/index.css";
 import Popup from "@pages/popup/Popup";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
+import Request from "./ui/Request";
 refreshOnUpdate("pages/popup");
 
 function init() {
@@ -12,7 +14,24 @@ function init() {
     throw new Error("Can not find #app-container");
   }
   const root = createRoot(appContainer);
-  root.render(<Popup />);
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
+  root.render(
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter>
+        <Routes>
+          <Route path="/" element={<Popup />} />
+          <Route path="/request" element={<Request />} />
+        </Routes>
+      </MemoryRouter>
+    </QueryClientProvider>
+  );
 }
 
 init();
