079a2b7f36a767f6fd846825319beb2376b25303
ww8007 <wshmin1234@gmail.com>
2023-05-17 02:45:01 +0900

    style: purple, grey 색상 추가

--- src/common/ui/styles/theme.css.ts
+++ src/common/ui/styles/theme.css.ts
@@ -1,5 +1,7 @@
 import { createGlobalTheme } from "@vanilla-extract/css";
 
+export type Color = keyof (typeof vars)["color"];
+
 export const vars = createGlobalTheme(":root", {
   color: {
     background: "#16191C",
@@ -9,14 +11,19 @@ export const vars = createGlobalTheme(":root", {
     scrollbarTrackBoxShadow: "inset 0px 0px 5px white",
     red: "#FF2A37",
     green: "#2EC885",
+    lightGreen: "#79EB3C",
     blue: "#4CA5FD",
     orange: "#FF952D",
     grey: "#323232",
+    darkGrey: "#202020",
     lightGrey: "#4F4F4F",
+    lightGrey2: "#8C8C8C",
     white: "#FFFFFF",
     whiteOrange: "#FFEDDC",
     whiteRed: "#FFE9EA",
     whiteBlue: "#E7F0F9",
+    purple: "#8B5AF2",
+    darkPurple: "#5F3DC4",
   },
   methodColors: {
     GET: "#4CA5FD",
