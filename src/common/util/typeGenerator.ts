b681d261bb015e1287365abec73a404f7958e769
ww8007 <wshmin1234@gmail.com>
2023-05-20 23:09:55 +0900

    feat: 배열 형태의 Response 처리 로직 추가

--- src/common/util/typeGenerator.ts
+++ src/common/util/typeGenerator.ts
@@ -2,35 +2,48 @@
 
 const toTsType = (value: any): string => {
   const jsType = typeof value;
-  if (jsType === "number" || jsType === "boolean" || value === null) {
+  if (jsType === "number" || jsType === "boolean" || value === null)
     return jsType;
-  } else if (Array.isArray(value)) {
+  else if (Array.isArray(value))
     return value.length > 0 ? `${toTsType(value[0])}[]` : "any[]";
-  } else if (jsType === "object") {
-    return "any";
-  } else {
-    return "string";
-  }
+  else if (jsType === "object") return "any";
+  else return "string";
 };
 
-const jsonToTs = (key: string, json: any): string[] => {
+const jsonToTs = (
+  key: string,
+  json: object,
+  parentIsArray = false
+): string[] => {
+  const capitalizedKey = key.charAt(0).toUpperCase() + key.slice(1);
   const interfaces = [
-    `export interface ${key.charAt(0).toUpperCase() + key.slice(1)} {\n`,
+    `export interface ${parentIsArray ? capitalizedKey : capitalizedKey} {\n`,
   ];
 
-  for (const key in json) {
-    if (Array.isArray(json[key]) && typeof json[key][0] === "object") {
-      interfaces[0] += `  ${key}: ${
+  if (Array.isArray(json)) {
+    if (typeof json[0] === "object") {
+      interfaces.push(...jsonToTs(`${key}Item`, json[0], true));
+      interfaces[0] += `  items: ${
         key.charAt(0).toUpperCase() + key.slice(1)
-      }[];\n`;
-      interfaces.push(...jsonToTs(key, json[key][0]));
-    } else if (typeof json[key] === "object" && json[key] !== null) {
-      interfaces[0] += `  ${key}: ${
-        key.charAt(0).toUpperCase() + key.slice(1)
-      };\n`;
-      interfaces.push(...jsonToTs(key, json[key]));
+      }Item[];\n`;
     } else {
-      interfaces[0] += `  ${key}: ${toTsType(json[key])};\n`;
+      interfaces[0] += `  items: ${toTsType(json[0])}[];\n`;
+    }
+  } else {
+    for (const key in json) {
+      if (Array.isArray(json[key]) && typeof json[key][0] === "object") {
+        interfaces[0] += `  ${key}: ${
+          key.charAt(0).toUpperCase() + key.slice(1)
+        }[];\n`;
+        interfaces.push(...jsonToTs(key, json[key][0], true));
+      } else if (typeof json[key] === "object" && json[key] !== null) {
+        interfaces[0] += `  ${key}: ${
+          key.charAt(0).toUpperCase() + key.slice(1)
+        };\n`;
+        interfaces.push(...jsonToTs(key, json[key]));
+      } else {
+        interfaces[0] += `  ${key}: ${toTsType(json[key])};\n`;
+      }
     }
   }
 
