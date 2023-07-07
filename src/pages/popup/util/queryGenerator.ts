9d309d46f1019c3c0c3a12f165c55cde3e919fb4
ww8007 <wshmin1234@gmail.com>
2023-07-07 23:33:55 +0900

    feat: reactQuery 코드 만드는 유틸 함수 추가

---
+++ src/pages/popup/util/queryGenerator.ts
@@ -0,0 +1,88 @@
+import { Method } from "axios";
+import { Parameters } from "../api/docs";
+import { typeConverter } from "./typeConverter";
+
+const generateQueryKey = (params: Parameters[]) => {
+  return params
+    .filter((param) => param.in !== "body")
+    .map((param) => param.name)
+    .join(",");
+};
+
+const generateInterface = (
+  apiFunctionName: string,
+  params: Parameters[],
+  queryKey: string
+) => {
+  const capitalizedFunctionName =
+    apiFunctionName.charAt(0).toUpperCase() + apiFunctionName.slice(1);
+
+  const paramsInterface = queryKey ? `${capitalizedFunctionName}Request` : "";
+
+  const interfaceItems = params
+    ? params
+        .filter((param) => param.in !== "body")
+        .map((param) => `  ${param.name}: ${typeConverter(param.schema.type)};`)
+        .join("\n")
+    : "";
+
+  let interfaceStr = "";
+  if (paramsInterface) {
+    interfaceStr = `
+export interface ${paramsInterface} {\n${interfaceItems}
+}`;
+  }
+
+  return { paramsInterface, interfaceStr };
+};
+
+export interface GenerateReactQueryHookProps {
+  api: {
+    method: Method;
+    params: Parameters[];
+  };
+  apiFunctionName: string;
+}
+
+const generateReactQueryHook = ({
+  api,
+  apiFunctionName,
+}: GenerateReactQueryHookProps): string => {
+  const { method, params } = api;
+
+  const capitalizedFunctionName =
+    apiFunctionName.charAt(0).toUpperCase() + apiFunctionName.slice(1);
+
+  const queryKey = generateQueryKey(params);
+
+  const { paramsInterface, interfaceStr } = generateInterface(
+    capitalizedFunctionName,
+    params,
+    queryKey
+  );
+
+  const queryKeysWithParams = queryKey
+    .split(",")
+    .map((key) => `params.${key}`)
+    .join(", ");
+  const queryKeyFunctionString = `const ${capitalizedFunctionName}Key = (params: ${paramsInterface}) => ['${method}', ${queryKeysWithParams}];`;
+
+  const queryKeyString = `${capitalizedFunctionName}Key(params)`;
+
+  if (method.toLowerCase() === "get") {
+    return `${interfaceStr}\n
+${queryKeyFunctionString}\n
+export const use${capitalizedFunctionName}Query = (params: ${paramsInterface}) => {
+  return useQuery(${queryKeyString}, async () => ${apiFunctionName});
+};
+`;
+  } else {
+    return `${interfaceStr}
+export const use${capitalizedFunctionName}Mutation = () => {
+  return useMutation(${apiFunctionName});
+};
+`;
+  }
+};
+
+export { generateReactQueryHook };
