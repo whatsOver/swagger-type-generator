295c8157cc9dd8987830efbf21a375e4c59e419f
ww8007 <wshmin1234@gmail.com>
2023-12-07 12:24:21 +0900

    feat: array type 지원 및 파일 업로드 기능 추가

--- src/pages/popup/hooks/useForm.ts
+++ src/pages/popup/hooks/useForm.ts
@@ -1,17 +1,42 @@
 import type { ChangeEvent } from "react";
 import { useState } from "react";
 
+type ValueType = string | File | (string | File)[];
+export interface FormValues {
+  [key: string]: ValueType;
+}
+
 const useForm = () => {
-  const [formValues, setFormValues] = useState<Record<string, any>>({});
+  const [formValues, setFormValues] = useState<FormValues>({});
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormValues((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const addArrayItem = (key: string, value: string | File) => {
+    setFormValues((prev) => {
+      if (!prev[key]) return { ...prev, [key]: [value] };
+      return {
+        ...prev,
+        [key]: [...(prev[key] as string[] | File[]), value],
+      };
+    });
+  };
+
+  const removeArrayItem = (key: string, index: number) => {
     setFormValues((prev) => ({
       ...prev,
-      [e.target.name]: e.target.value,
+      [key]: (prev[key] as string[]).filter((_, i) => i !== index),
     }));
   };
 
-  return { formValues, handleChange, setFormValues };
+  return {
+    formValues,
+    handleChange,
+    setFormValues,
+    handleArray: { addArrayItem, removeArrayItem },
+  };
 };
 
 export default useForm;
