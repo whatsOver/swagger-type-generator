a5d14e905899322a8edff6a4a4f70b6d3f57b318
ww8007 <wshmin1234@gmail.com>
2024-01-02 04:37:47 +0900

    feat: 보라색 버튼 추가

--- src/common/ui/Button.tsx
+++ src/common/ui/Button.tsx
@@ -1,38 +1,40 @@
-import { ButtonHTMLAttributes, FunctionComponent, ReactNode } from "react";
+import React, { forwardRef, ButtonHTMLAttributes, ReactNode } from "react";
 import {
   blueButtonStyles,
   buttonStyles,
   greenButtonStyles,
   orangeButtonStyles,
+  purpleButtonStyles,
   redButtonStyles,
 } from "./styles/button.css";
 import classNames from "classnames";
 
 export type ButtonProps = {
   children: ReactNode;
-  color?: "blue" | "green" | "red" | "orange" | "default";
+  color?: "blue" | "green" | "red" | "orange" | "purple" | "default";
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button: FunctionComponent<ButtonProps> = ({
-  children,
-  type = "button",
-  color = "default",
-  ...rest
-}) => {
-  return (
-    <button
-      type={type}
-      className={classNames(buttonStyles, {
-        [blueButtonStyles]: color === "blue",
-        [greenButtonStyles]: color === "green",
-        [redButtonStyles]: color === "red",
-        [orangeButtonStyles]: color === "orange",
-      })}
-      {...rest}
-    >
-      {children}
-    </button>
-  );
-};
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, type = "button", color = "default", ...rest }, ref) => {
+    return (
+      <button
+        type={type}
+        ref={ref}
+        className={classNames(buttonStyles, {
+          [blueButtonStyles]: color === "blue",
+          [greenButtonStyles]: color === "green",
+          [redButtonStyles]: color === "red",
+          [orangeButtonStyles]: color === "orange",
+          [purpleButtonStyles]: color === "purple",
+        })}
+        {...rest}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
 
 export default Button;
