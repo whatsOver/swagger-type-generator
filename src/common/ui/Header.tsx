1799b00923474f2bfab2093434103cbfae01e564
ww8007 <wshmin1234@gmail.com>
2024-01-02 04:38:53 +0900

    feat: 뒤로가기 버튼과 오른쪽 버튼 추가할 수 있게 확장

--- src/common/ui/Header.tsx
+++ src/common/ui/Header.tsx
@@ -3,20 +3,30 @@ import { BiChevronLeft } from "react-icons/bi";
 import { headerStyle } from "./styles/header.css";
 import { vars } from "./styles/theme.css";
 interface HeaderProps {
-  title?: string;
+  showBackButton?: boolean;
+  leftButton?: React.ReactNode;
+  rightButton?: React.ReactNode;
 }
 
-const Header = ({ title }: HeaderProps) => {
+const Header = ({
+  showBackButton = false,
+  leftButton,
+  rightButton,
+}: HeaderProps) => {
   const navigate = useNavigate();
   return (
     <header className={headerStyle.header}>
       <div className={headerStyle.left}>
-        <BiChevronLeft
-          color={vars.color.white}
-          size={35}
-          onClick={() => navigate(-1)}
-        />
+        {showBackButton && (
+          <BiChevronLeft
+            color={vars.color.white}
+            size={35}
+            onClick={() => navigate(-1)}
+          />
+        )}
+        {leftButton}
       </div>
+      <div className={headerStyle.right}>{rightButton}</div>
     </header>
   );
 };
