9e95377bbe25961cc9835705fa44cd4e2e66ab94
ww8007 <wshmin1234@gmail.com>
2024-05-16 02:22:17 +0900

    feat: Header의 뒤로가기 동작 추가 및 headerTitle 추가

--- src/common/ui/Header.tsx
+++ src/common/ui/Header.tsx
@@ -1,19 +1,32 @@
-import { useNavigate } from "react-router-dom";
 import { BiChevronLeft } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 import { headerStyle } from "./styles/header.css";
 import { vars } from "./styles/theme.css";
 interface HeaderProps {
   showBackButton?: boolean;
+  backTo?: string;
+  headerTitle?: string;
   leftButton?: React.ReactNode;
   rightButton?: React.ReactNode;
 }
 
 const Header = ({
   showBackButton = false,
+  backTo,
+  headerTitle,
   leftButton,
   rightButton,
 }: HeaderProps) => {
   const navigate = useNavigate();
+
+  const onClickBack = () => {
+    if (backTo) {
+      navigate(backTo);
+      return;
+    }
+    navigate(-1);
+  };
+
   return (
     <header className={headerStyle.header}>
       <div className={headerStyle.left}>
@@ -21,10 +34,13 @@ const Header = ({
           <BiChevronLeft
             color={vars.color.white}
             size={35}
-            onClick={() => navigate(-1)}
+            onClick={onClickBack}
           />
         )}
         {leftButton}
+        {headerTitle && (
+          <span className={headerStyle.headerTitle}>{headerTitle}</span>
+        )}
       </div>
       <div className={headerStyle.right}>{rightButton}</div>
     </header>
