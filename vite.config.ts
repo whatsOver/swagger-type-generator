c2ef1340840f88f0dd6a11c8164199e2fa00de66
ww8007 <wshmin1234@gmail.com>
2024-07-04 22:59:55 +0900

    setting: fsd 사용을 위한 상대 경로 정보 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react";
 import path, { resolve } from "path";
-import makeManifest from "./utils/plugins/make-manifest";
-import customDynamicImport from "./utils/plugins/custom-dynamic-import";
-import addHmr from "./utils/plugins/add-hmr";
-import manifest from "./manifest";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { PreRenderedChunk } from "rollup";
 import copy from "rollup-plugin-copy";
+import { defineConfig } from "vite";
+import manifest from "./manifest";
+import addHmr from "./utils/plugins/add-hmr";
+import customDynamicImport from "./utils/plugins/custom-dynamic-import";
+import makeManifest from "./utils/plugins/make-manifest";
 
 const root = resolve(__dirname, "src");
 const pagesDir = resolve(root, "pages");
@@ -26,7 +26,11 @@ export default defineConfig({
     alias: {
       "@src": root,
       "@assets": assetsDir,
-      "@pages": pagesDir,
+      "@/app": resolve(root, "app"),
+      "@/pages": resolve(root, "pages"),
+      "@/shared": resolve(root, "shared"),
+      "@/widgets": resolve(root, "widgets"),
+      "@/features": resolve(root, "features"),
     },
   },
   plugins: [
