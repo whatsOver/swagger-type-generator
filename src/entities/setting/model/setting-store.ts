51d39da2e0a4f8359cd30aa7b71f5fd336eb02b2
ww8007 <wshmin1234@gmail.com>
2024-07-08 21:24:44 +0900

    fix: export default 방식에서 export로 변경

--- src/entities/setting/model/setting-store.ts
+++ src/entities/setting/model/setting-store.ts
@@ -1,14 +1,11 @@
 import { create } from "zustand";
-
 interface SettingStore {
   withReactQuery: boolean;
   toggleReactQuery: () => void;
 }
 
-const useSettingStore = create<SettingStore>((set) => ({
+export const useSettingStore = create<SettingStore>((set) => ({
   withReactQuery: false,
   toggleReactQuery: () =>
     set((state) => ({ withReactQuery: !state.withReactQuery })),
 }));
-
-export default useSettingStore;
